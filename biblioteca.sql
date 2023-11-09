a2d2efe44e78748013a08d2333be51e66c2d376f
egisell963 <134333035+egisell963@users.noreply.github.com>
2023-11-09 11:59:59 -0500

    Se agrega la BD
    Se agrega la base de datos, la cual contiene los registros principales para el funcionamiento correcto de la aplicación.

--- biblioteca.sql
+++ biblioteca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-10-2023 a las 16:28:04
+-- Tiempo de generación: 09-11-2023 a las 17:57:19
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.2.0
 
@@ -128,7 +128,9 @@ CREATE TABLE `auth_user` (
 --
 
 INSERT INTO `auth_user` (`id`, `password`, `last_login`, `is_superuser`, `username`, `first_name`, `last_name`, `email`, `is_staff`, `is_active`, `date_joined`) VALUES
-(1, 'pbkdf2_sha256$600000$ezSnMBO0EZxvxIzqLPgjA8$dMcYLvtnBkpbIvOmWQ4o0+KFGGTJCgg371rzXboYT4k=', '2023-10-27 13:46:44.634513', 1, 'gisell', '', '', 'egisell@gmail.com', 1, 1, '2023-10-26 14:09:20.327457');
+(1, 'pbkdf2_sha256$600000$ezSnMBO0EZxvxIzqLPgjA8$dMcYLvtnBkpbIvOmWQ4o0+KFGGTJCgg371rzXboYT4k=', '2023-11-08 16:29:48.169619', 1, 'gisell', '', '', 'egisell@gmail.com', 1, 1, '2023-10-26 14:09:20.327457'),
+(2, 'pbkdf2_sha256$600000$uVrn1LI7jd1TGlC25PoJgb$SmeNdISh/SK7Q93Bx343+/xvMzGslMMd5zqm1F0Hqsk=', '2023-11-08 15:32:01.520689', 0, 'Brenda123', '', '', 'brendaca@gmail.com', 0, 1, '2023-11-03 16:30:00.084547'),
+(3, 'pbkdf2_sha256$600000$vK7bxrsIlMhsdMk3rMNikw$vZm4hwtXP17DXOYA25/29DQJMUDMWDcYBf66TVsK1wE=', '2023-11-05 19:47:09.026518', 0, 'Esteban123', '', '', 'esteban1@gmail.com', 0, 1, '2023-11-05 19:47:05.703528');
 
 -- --------------------------------------------------------
 
@@ -248,7 +250,9 @@ INSERT INTO `django_migrations` (`id`, `app`, `name`, `applied`) VALUES
 (17, 'auth', '0012_alter_user_first_name_max_length', '2023-10-26 04:17:09.812871'),
 (18, 'libreria', '0001_initial', '2023-10-26 04:17:09.873874'),
 (19, 'sessions', '0001_initial', '2023-10-26 04:17:10.022883'),
-(20, 'libreria', '0002_rol_usuario_libro_usuario', '2023-10-26 21:49:50.339272');
+(20, 'libreria', '0002_rol_usuario_libro_usuario', '2023-10-26 21:49:50.339272'),
+(21, 'libreria', '0003_libro_estado_alter_libro_usuario_id_usuario_and_more', '2023-11-08 19:17:51.106689'),
+(22, 'libreria', '0004_alter_libro_estado', '2023-11-08 19:32:14.669124');
 
 -- --------------------------------------------------------
 
@@ -267,7 +271,11 @@ CREATE TABLE `django_session` (
 --
 
 INSERT INTO `django_session` (`session_key`, `session_data`, `expire_date`) VALUES
-('2k1sdvn47wv277sj8tolibbiuhjk4ddn', '.eJxVjDsOwjAQBe_iGlle_0NJzxms9a7BAeRIcVIh7k4ipYD2zcx7i4TrUtPay5xGFmcB4vS7ZaRnaTvgB7b7JGlqyzxmuSvyoF1eJy6vy-H-HVTsdaujAVLkjAeKFjMQaR108caZGBxgsI6Zg9VA5TY4D0apgX2hDTMaLz5fwJE3Cw:1qw16d:fZRHPcencWSQMbxrT8z4Pg5rDV9gTUvLnh9ClT2W_Gg', '2023-11-09 14:13:03.965129');
+('2k1sdvn47wv277sj8tolibbiuhjk4ddn', '.eJxVjDsOwjAQBe_iGlle_0NJzxms9a7BAeRIcVIh7k4ipYD2zcx7i4TrUtPay5xGFmcB4vS7ZaRnaTvgB7b7JGlqyzxmuSvyoF1eJy6vy-H-HVTsdaujAVLkjAeKFjMQaR108caZGBxgsI6Zg9VA5TY4D0apgX2hDTMaLz5fwJE3Cw:1qw16d:fZRHPcencWSQMbxrT8z4Pg5rDV9gTUvLnh9ClT2W_Gg', '2023-11-09 14:13:03.965129'),
+('4a29k181uvyur7dnrng1y7x6c2ou8g5f', '.eJxVjEEOwiAQRe_C2hAKdACX7nsGMjBTqRqalHZlvLtt0oVu33v_v0XEbS1xa7zEicRVGHH5ZQnzk-sh6IH1Pss813WZkjwSedomh5n4dTvbv4OCrexrNo4gsEcIisfROdN7djbYTvvEXkFvFARLBJA7YKUUaa-tsTvVSaP4fAHHnDaZ:1qzj5R:b-Sx6Z6tVIw3SVCAT8vIgidsu9IaUIuj0fvihDaGINs', '2023-11-19 19:47:09.045519'),
+('6lb4cp0mlj3hu7z14y2eujx2fp2mgi3a', '.eJxVjDsOwjAQBe_iGlle_0NJzxms9a7BAeRIcVIh7k4ipYD2zcx7i4TrUtPay5xGFmcB4vS7ZaRnaTvgB7b7JGlqyzxmuSvyoF1eJy6vy-H-HVTsdaujAVLkjAeKFjMQaR108caZGBxgsI6Zg9VA5TY4D0apgX2hDTMaLz5fwJE3Cw:1r0lR6:Bz3jjVQnjYKDV6CLeNeEdDcH1S_8umKhzOSvz0xsHko', '2023-11-22 16:29:48.181618'),
+('bskp1ipby73wswk5fi7jin632v1vfmvu', '.eJxVjDsOwjAQBe_iGln-setQ0ucM1tre4ACypTipEHeHSCmgfTPzXiLQtpawdV7CnMVFGHH63SKlB9cd5DvVW5Op1XWZo9wVedAux5b5eT3cv4NCvXxrNzAiMmTHGbR3EXTyqByAOUd2MaMBS37wk3WEysDkWVk0WtukmVi8P8gDNyM:1qziwJ:mXpDtrcw98OSkLvhWeZ92wcz549son4K1VOQME87-_s', '2023-11-19 19:37:43.187716'),
+('n8ke312kea85ifpvv868tudksk4lwu39', '.eJxVjDsOwjAQBe_iGlle_0NJzxms9a7BAeRIcVIh7k4ipYD2zcx7i4TrUtPay5xGFmcB4vS7ZaRnaTvgB7b7JGlqyzxmuSvyoF1eJy6vy-H-HVTsdaujAVLkjAeKFjMQaR108caZGBxgsI6Zg9VA5TY4D0apgX2hDTMaLz5fwJE3Cw:1qzivN:DthMAPDqD2SUOSKDNx51BEGwzzC0Q8rE5bZHv97WcTA', '2023-11-19 19:36:45.829765');
 
 -- --------------------------------------------------------
 
@@ -281,18 +289,27 @@ CREATE TABLE `libreria_libro` (
   `autor` varchar(100) NOT NULL,
   `descripcion` longtext DEFAULT NULL,
   `fecha` int(11) DEFAULT NULL,
-  `cantidad` int(11) DEFAULT NULL
+  `cantidad` int(11) DEFAULT NULL,
+  `estado` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `libreria_libro`
 --
 
-INSERT INTO `libreria_libro` (`id_libro`, `titulo`, `autor`, `descripcion`, `fecha`, `cantidad`) VALUES
-(1, 'La ilíada', 'Homero', 'La Ilíada es un poema de género épico que trata el asedio de la ciudad de Troya por los griegos para rescatar a Helena, esposa del rey Melando, la cual fue raptada por París, príncipe troyano.', 1892, 4),
-(2, 'Fundamentos de programación', 'José Antonio Cerrada Somolinos, ‎Manuel E. Collado Machuca', 'El texto esta diseñado para poder seguir una asignatura de Fundamentos de Programación de primer curso de Grado en Informática, pero es un texto introductorio que puede seguir cualquier lector que tenga conocimientos generales de ...', 2010, 10),
-(3, 'Programación para principiantes', 'Alexander Cane', 'Esto no es cualquier tipo de libro de programación que aprieta todo en un libro de 300 páginas; hay Wikipedia para eso! Programación para principiantes es una hoja de ruta para cualquier persona, joven o mayor, que busca una manera de ...', 2019, 2),
-(8, 'php', 'esteban', 'php lenguaje', 2021, 9);
+INSERT INTO `libreria_libro` (`id_libro`, `titulo`, `autor`, `descripcion`, `fecha`, `cantidad`, `estado`) VALUES
+(2, 'Fundamentos de programación', 'José Antonio Cerrada Somolinos, ‎Manuel E. Collado Machuca', 'El texto esta diseñado para poder seguir una asignatura de Fundamentos de Programación de primer curso de Grado en Informática, pero es un texto introductorio que puede seguir cualquier lector que tenga conocimientos generales de ...', 2010, 9, 'activo'),
+(3, 'Programación para principiantes', 'Alexander Cane', 'Esto no es cualquier tipo de libro de programación que aprieta todo en un libro de 300 páginas; hay Wikipedia para eso! Programación para principiantes es una hoja de ruta para cualquier persona, joven o mayor, que busca una manera de ...', 2019, 1, 'activo'),
+(8, 'PHP', 'Esteban Gil', 'Lenguaje de programación para paginas web', 2021, 9, 'eliminado'),
+(14, 'Curso de python', 'Sebastian', 'Curso interactivo del lenguaje de programación Python', 2005, 2, 'activo'),
+(15, 'Lenguaje C', 'Enrique Vicente Bonet Esteban', 'Esta obra pretende explorar la lógica de programación a través del. Lenguaje C y los diagramas de flujo, con el propósito de ofrecer una.', 1992, 1, 'activo'),
+(16, 'JAVA', 'Manuel Carriño', 'Explica ampliamente los conceptos básicos de la programación en el lenguaje JAVA.', 2000, 5, 'activo'),
+(17, 'C++', 'Julio Mocada', 'Conceptos básicos de programación en el lenguaje de programación C++', 2004, 8, 'activo'),
+(18, 'php', 'Carlos Salceda', 'Programación orientada a objetos en php', 1994, 10, 'activo'),
+(19, 'La ilíada', 'Homero', 'La iliada', 1233, 12, ''),
+(20, 'La ilíada', 'Homero', 'bwdbkj QDW', 12324, 12, ''),
+(21, 'La ilíada', 'Homero', '12341qwwdqef', 12324, 32, ''),
+(22, 'La ilíada', 'Homero', '123456789', 1234, 12, 'eliminado');
 
 -- --------------------------------------------------------
 
@@ -306,6 +323,27 @@ CREATE TABLE `libreria_libro_usuario` (
   `id_libro_id` int(11) NOT NULL,
   `id_usuario_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `libreria_libro_usuario`
+--
+
+INSERT INTO `libreria_libro_usuario` (`id_libro_usuario`, `estado`, `id_libro_id`, `id_usuario_id`) VALUES
+(1, 'devuelto', 2, 1),
+(2, 'devuelto', 8, 1),
+(3, 'devuelto', 3, 1),
+(4, 'devuelto', 14, 2),
+(6, 'devuelto', 3, 2),
+(7, 'devuelto', 15, 1),
+(8, 'alquilado', 16, 1),
+(9, 'alquilado', 16, 2),
+(10, 'devuelto', 3, 2),
+(12, 'alquilado', 14, 3),
+(13, 'alquilado', 16, 3),
+(14, 'alquilado', 3, 3),
+(15, 'alquilado', 17, 2),
+(16, 'alquilado', 2, 2),
+(17, 'alquilado', 15, 2);
 
 -- --------------------------------------------------------
 
@@ -438,7 +476,7 @@ ALTER TABLE `libreria_libro`
 ALTER TABLE `libreria_libro_usuario`
   ADD PRIMARY KEY (`id_libro_usuario`),
   ADD KEY `libreria_libro_usuar_id_libro_id_12f8a3f6_fk_libreria_` (`id_libro_id`),
-  ADD KEY `libreria_libro_usuar_id_usuario_id_c9207ad2_fk_libreria_` (`id_usuario_id`);
+  ADD KEY `libreria_libro_usuario_id_usuario_id_c9207ad2_fk_auth_user_id` (`id_usuario_id`);
 
 --
 -- Indices de la tabla `libreria_rol`
@@ -451,6 +489,7 @@ ALTER TABLE `libreria_rol`
 --
 ALTER TABLE `libreria_usuario`
   ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `libreria_usuario_correo_53935787_uniq` (`correo`) USING HASH,
   ADD KEY `libreria_usuario_rol_id_92c49136_fk_libreria_rol_id_rol` (`rol_id`);
 
 --
@@ -479,7 +518,7 @@ ALTER TABLE `auth_permission`
 -- AUTO_INCREMENT de la tabla `auth_user`
 --
 ALTER TABLE `auth_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `auth_user_groups`
@@ -509,19 +548,19 @@ ALTER TABLE `django_content_type`
 -- AUTO_INCREMENT de la tabla `django_migrations`
 --
 ALTER TABLE `django_migrations`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de la tabla `libreria_libro`
 --
 ALTER TABLE `libreria_libro`
-  MODIFY `id_libro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_libro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de la tabla `libreria_libro_usuario`
 --
 ALTER TABLE `libreria_libro_usuario`
-  MODIFY `id_libro_usuario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_libro_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de la tabla `libreria_rol`
@@ -578,7 +617,7 @@ ALTER TABLE `django_admin_log`
 --
 ALTER TABLE `libreria_libro_usuario`
   ADD CONSTRAINT `libreria_libro_usuar_id_libro_id_12f8a3f6_fk_libreria_` FOREIGN KEY (`id_libro_id`) REFERENCES `libreria_libro` (`id_libro`),
-  ADD CONSTRAINT `libreria_libro_usuar_id_usuario_id_c9207ad2_fk_libreria_` FOREIGN KEY (`id_usuario_id`) REFERENCES `libreria_usuario` (`id_usuario`);
+  ADD CONSTRAINT `libreria_libro_usuario_id_usuario_id_c9207ad2_fk_auth_user_id` FOREIGN KEY (`id_usuario_id`) REFERENCES `auth_user` (`id`);
 
 --
 -- Filtros para la tabla `libreria_usuario`
